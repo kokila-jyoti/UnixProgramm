50a6beaa224cda7cf6ee3f26ee3d52330cbb1d8e
kokila-jyoti <66548656+kokila-jyoti@users.noreply.github.com>
2020-06-07 14:26:41 +0530

    Update guessinggame.sh

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,6 +1,6 @@
 function guessing()
 {
-    true_ans=$(ls -l |grep "^-"|wc -l)
+    count=$(ls -l |grep "^-"|wc -l)
     flag=0
     while [ $flag == 0 ]
     do
@@ -8,16 +8,16 @@ function guessing()
         read  number
         if [ $number -lt $true_ans ]
         then
-            echo "your guess is Less then the true number"
-        elif [ $number -gt $true_ans ]
+            echo "your guess is Lesser"
+        elif [ $number -gt $count ]
         then
-            echo "your guess is Greater then the true number"
+            echo "your guess is Greater"
         else
             echo " congratulation,you are right!"
             $flag=1
-            break;
+            break
         fi
     done
 }
-echo "guess the files number in the current directory!"
+echo "Guessing Game!"
 guessing
